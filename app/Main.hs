6da1f28563f4960a9aabfae240a3ea561adfcf61
spacedome <spacedome@users.noreply.github.com>
2025-02-01 21:36:00 -0500

    runge example plot

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,25 @@
 module Main where
 
-import Approx.List
-
--- import qualified Data.Vector as V
-
--- x^4
-chebX4 :: Cheb
-chebX4 = Cheb [0.375, 0.0, 0.5, 0.0, 0.125]
-x4 :: Function
-x4 = Function (** 4)
+import Approx.Cheb
+import Approx.List (extremalChebNodes)
+import Graphics.Rendering.Chart.Backend.Diagrams
+import Graphics.Rendering.Chart.Easy
 
 main :: IO ()
-main = do
-  print (computeCheb x4 4)
+main = plotRunge
+
+-- plot a Chebyshev interpolation of the classic function
+-- used to demonstrate Runge's phenomenon.
+plotRunge :: IO ()
+plotRunge = toFile def "runge.png" $ do
+  layout_title .= "1 / (1 + 25x^2)"
+  setColors [opaque blue, opaque green, opaque red]
+  plot (line "f" [zip linrange (fmap runge linrange)])
+  plot (line "cheb14" [zip linrange interp])
+  plot (points "points" (zip nodes (fmap runge nodes)))
+  where
+    linrange = [x / 100 | x <- [-100 .. 100]] :: [Double]
+    runge x = 1 / (1 + 25 * x ** 2)
+    rungeCheb = computeCheb (Function runge) 14
+    interp = fmap (clenshaw (getChebCoef rungeCheb)) linrange
+    nodes = Approx.List.extremalChebNodes 14
